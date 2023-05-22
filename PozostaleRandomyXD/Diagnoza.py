19ee3fad9deb2d25fab737008e90df8f3754cfe4
27tr <114559221+MCDream3@users.noreply.github.com>
2023-05-22 17:31:16 +0200

    Diagnoza ale nie wszystkie zadania są zrobione/zrobione poprawnie

---
+++ PozostaleRandomyXD/Diagnoza.py
@@ -0,0 +1,63 @@
+#Zad 3    +
+
+# suma = 0
+# for i in range(100,1000):
+#   if i % 2 == 0:
+#     suma = suma + i
+# print(suma)
+
+
+#Zad 4    +
+# for j in range(10,100):
+#   if j % 2 == 0 and j % 13 == 0:
+#     print(j)
+
+#Zad 5    +
+
+# n = int(input())
+# ilosc = 0
+# for k in range(1,1+n):
+#   if n % k == 0:
+#     ilosc += 1
+# print(ilosc)
+
+#Zad 6    -
+
+# n = int(input())
+
+#Zad 7    +-
+
+# from math import gcd
+
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# d = int(input())
+# NWW = gcd(b, d) * b
+# l_a = a *(NWW // b)
+# l_c = c *(NWW // d)
+# dodaj = l_a + l_c
+# print(f"{a}/{b} + {c}/{d} = {l_a}/{NWW} + {l_c}/{NWW} = {dodaj}/{NWW}")
+
+#Zad 8    +
+
+# s = input()
+# il_parz = 0
+# for l in s:
+#   if ord(l) % 2 != 0:
+#     print(ord(l))
+#     il_parz += 1
+# print(il_parz)
+
+#Zad 9    -
+
+# s1 = input()
+# s2 = input()
+# s3 = input()
+
+#*ta część została dorobiona po lekcji
+
+#if len(s1) == 20 and len(s2) == 20 and len(s2) == 20:
+#   print("są takiej samej długości")
+#else:
+#   #   print("nie są takiej samej długości")
